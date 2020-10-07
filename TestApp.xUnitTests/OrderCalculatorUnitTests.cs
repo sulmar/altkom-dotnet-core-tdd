c7d6be985bbb42bc5e794d0fa35f4cc9dd068be4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-07 14:33:33 +0200

    Refaktoring

---
+++ TestApp.xUnitTests/OrderCalculatorUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestApp.Fundamentals;
+using Xunit;
+using Xunit.Sdk;
+
+namespace TestApp.xUnitTests
+{
+    public class OrderCalculatorUnitTests
+    {
+        private IOrderShippingCalculator orderShippingCalculator;
+
+        public OrderCalculatorUnitTests()
+        {
+            orderShippingCalculator = new OrderCalculator();
+        }
+
+        [Fact]
+        public void CalculateShippingCost_TotalAmountBelow1000_ReturnsNotZero()
+        {
+            // Arrange
+            Order order = new Order { TotalAmount = 999 };
+
+            // Act
+            var result = orderShippingCalculator.CalculateShippingCost(order);
+
+            // Assert
+            Assert.Equal(9.99m, result);
+        }
+
+
+        [Fact]
+        public void CalculateShippingCost_TotalAmountEqualOrAbove1000_ReturnsZero()
+        {
+            // Arrange
+            Order order = new Order { TotalAmount = 1000 };
+
+            // Act
+            var result = orderShippingCalculator.CalculateShippingCost(order);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void CalculateShippingCost_OrderNull_ThrowsArgumentNullException()
+        {
+           // Act
+
+            Action act = () => orderShippingCalculator.CalculateShippingCost(null);
+
+            //
+            Assert.Throws<ArgumentNullException>(act);
+        }
+
+
+    }
+}
--- TestApp/Fundamentals/OrderSender.cs
+++ TestApp/Fundamentals/OrderSender.cs
@@ -15,7 +15,7 @@ namespace TestApp.Fundamentals
     {
         public string Number { get; set; }
         public DateTime OrderDate { get; set; }
-        public decimal TotalAmount { get; set; }        
+        public decimal TotalAmount { get; set; }
     }
 
     #endregion
@@ -46,26 +46,81 @@ namespace TestApp.Fundamentals
 
     */
 
-    public class OrderSender
+    public interface IOrderShippingCalculator
     {
-        public void Send(Order order)
-        {
-            decimal shippingCost = CalculateShippingCost(order);
+        decimal CalculateShippingCost(Order order);
+    }
 
-            SendEmail($"Koszt dostawy {shippingCost}");
-        }
+    public interface IOrderTotalCalculator
+    {
+        decimal CalculateTotalCost(Order order);
+    }
 
-        private void SendEmail(string messsage)
+    public class OrderCalculator : IOrderShippingCalculator, IOrderTotalCalculator
+    {
+        public decimal CalculateShippingCost(Order order)
         {
-            Console.WriteLine($"Sending {messsage}...");
-        }
+            if (order == null)
+                throw new ArgumentNullException(nameof(Order));
 
-        private decimal CalculateShippingCost(Order order)
-        {
-            if (order.TotalAmount > 1000)
+            if (order.TotalAmount >= 1000)
                 return 0;
             else
                 return 9.99m;
         }
+
+        public decimal CalculateTotalCost(Order order)
+        {
+            return order.TotalAmount * 0.1m;
+        }
+    }
+
+    public class Message
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Content { get; set; }
+    }
+
+    public interface IMessageService
+    {
+        void Send(Message message);
+    }
+
+    public class EmailMessageService : IMessageService
+    {
+        public void Send(Message message)
+        {
+            SendEmail(message.From, message.To, message.Content);
+        }
+
+        private void SendEmail(string from, string to, string message)
+        {
+            Console.WriteLine($"Sending {message}...");
+        }
+    }
+
+    public class OrderSender
+    {
+        private readonly IOrderShippingCalculator orderShippingCalculator;
+        private readonly IMessageService messageService;
+
+        public OrderSender(IOrderShippingCalculator orderShippingCalculator, IMessageService messageService)
+        {
+            this.orderShippingCalculator = orderShippingCalculator ?? throw new ArgumentNullException(nameof(orderShippingCalculator));
+            this.messageService = messageService;
+        }
+
+        public void Send(Order order)
+        {
+            decimal shippingCost = orderShippingCalculator.CalculateShippingCost(order);
+
+            Message message = new Message { Content = $"Koszt dostawy {shippingCost}" };
+
+            messageService.Send(message);
+        }
+
+     
+       
     }
 }
