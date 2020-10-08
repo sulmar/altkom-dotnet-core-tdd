c5016bda89260839d907da137c7ad746ed0d8409
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-08 10:28:53 +0200

    Fluent-Api

---
+++ TestApp.FluentAssertionsUnitTests/FluentPhone.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp.FluentAssertionsUnitTests
+{
+    public class Phone
+    {
+        public void Call(string fromNumber, string toNumber, string subject = "")
+        {
+            Console.WriteLine($"Calling {fromNumber} to {toNumber}");
+        }
+
+        public void Hangout()
+        {
+
+        }
+    }
+
+    public interface IFrom
+    {
+        ITo From(string number);
+    }
+
+    public interface ITo : ISubject, ICall
+    {
+        ITo To(string number);
+    }
+
+    public interface ISubject : ICall
+    {
+        ICall WithSubject(string subject);
+    }
+
+    public interface ICall
+    {
+        void Call();
+    }
+
+
+    public class FluentPhone : IFrom, ITo, ICall, ISubject
+    {
+        private string fromNumber;
+        private ICollection<string> toNumbers;
+        private string subject;
+
+        public static IFrom Hangup()
+        {
+            return new FluentPhone();            
+        }
+
+        public ITo From(string number)
+        {
+            this.fromNumber = number;
+            return this;
+        }
+
+        public ITo To(string number)
+        {
+            this.toNumbers.Add(number);
+            return this;
+        }
+
+        public ICall WithSubject(string subject)
+        {
+            this.subject = subject;
+            return this;
+        }
+
+        public void Call()
+        {
+            foreach (var toNumber in toNumbers)
+            {
+                if (string.IsNullOrEmpty(subject))
+                    Console.WriteLine($"Calling {fromNumber} to {toNumber}");
+                else
+                    Console.WriteLine($"Calling {fromNumber} to {toNumber} subject {subject}");
+            }
+        }
+    }
+}
---
+++ TestApp.FluentAssertionsUnitTests/FluentPhoneUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace TestApp.FluentAssertionsUnitTests
+{
+    public class FluentPhoneUnitTests
+    {
+        
+        [Fact(Skip = "no bo tak")]
+        public void Call_WhenCalled_ReturnsCall()
+        {
+            FluentPhone
+                .Hangup()
+                .From("555-666-777")
+                .To("555-888-999")
+                .To("555-777-888")
+                .WithSubject("TDD")
+                .Call();
+        }
+    }
+}
